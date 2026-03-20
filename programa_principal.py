c182e344104559e133f6dbb2aacf5b0e1c0a97da
matilde-urrestarazu <murrestarazuromero@udesa.edu.ar>
2026-03-20 10:41:41 -0300

    cambie el print

--- programa_principal.py
+++ programa_principal.py
@@ -4,7 +4,7 @@ lista = funciones_habitos.registrar_habitos()
 
 resultado = funciones_habitos.analizar_habitos(lista)
 
-print("Resumen de actividades: ")
+print("Resumen de actividades:")
 
 for actividad, cantidad in resultado.items():
-    print(actividad, ":", cantidad)
+    print(f"{actividad} : {cantidad}")
